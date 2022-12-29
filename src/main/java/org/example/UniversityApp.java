db308ff31da32dbb5a6e96288e998b1315ccb49b
Andrii Kovtun <kovtun.ao@gmail.com>
2022-12-29 16:42:21 +0200

    Add UniversityApp.java

---
+++ src/main/java/org/example/UniversityApp.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UniversityApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityApp.class, args);
+    }
+
+}
