f020a2edff3e4386895b19e5270a88afe2cdfef9
Andrii Kovtun <kovtun.ao@gmail.com>
2022-12-29 16:43:47 +0200

    Add repository layer

---
+++ src/main/java/org/example/repository/BuildingRepository.java
@@ -0,0 +1,10 @@
+package org.example.repository;
+
+import org.example.entity.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BuildingRepository extends JpaRepository<Building, Long> {
+
+}
